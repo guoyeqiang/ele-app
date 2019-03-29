26cf3367cdb68c2ed5541f82d3aecec2bc9b33b7
guoyeqiang <mailforqiang7@gmail.cn>
2019-03-29 14:05:52 +0800

    初始化ele-app.

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    outputDir: 'dist',
+    assetsDir: 'assets',
+    lintOnSave: false,
+    devServer: {
+        open: true,
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: {
+            // 配置跨域
+            '/api': {
+                target: 'https://ele-interface.herokuapp.com/api/',
+                ws: true,
+                changOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+        before: app => {}
+    }
+};
